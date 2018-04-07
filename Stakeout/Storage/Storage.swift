28c765c006bf157dd51413b5c13e29decb46d1d8
Luke Stringer <luke@lukestringer.me>
2018-04-07 23:44:45 +0100

    Add documentation for Storage and Storable protocols

--- Stakeout/Storage/Storage.swift
+++ Stakeout/Storage/Storage.swift
@@ -8,61 +8,106 @@
 
 import Foundation
 
+
+/// Conform to the Storage protocol to faciltate the storage of a Storable entity.
 protocol Storage {
-    associatedtype Entity: Storable
-    
-    static var key: String { get }
-    
-    func add(_ entity: Entity)
-    func remove(_ entity: Entity)
-    func all() -> [Entity]
-    func reset()
-    func didUpdate(to entities: [Entity])
+	
+	/// The type of entity to store.
+	associatedtype Entity: Storable
+	
+	/// The unique key used as a namespace for the storage.
+	static var key: String { get }
+	
+	
+	/// Add an entity into storage.
+	///
+	/// - Parameter entity: The entity to store.
+	func add(_ entity: Entity)
+	
+	
+	/// Remove an entity from storage if it exists.
+	///
+	/// - Parameter entity: The entity to remove from storage.
+	func remove(_ entity: Entity)
+	
+	
+	/// Get all the stored entities.
+	///
+	/// - Returns: All the stored entities.
+	func all() -> [Entity]
+	
+	
+	/// Reset the storage by removing any stored entities
+	func reset()
+	
+	
+	/// Callback function, called when the storage is updated (entities are added or removed).
+	///
+	/// - Parameter entities: The entities currently in storage.
+	func didUpdate(to entities: [Entity])
 }
 
 
+/// Conform to the Storage protocol to facilitate encoding a type for storage, and decoding a type back out of storage.
 protocol Storable {
-    func encode() -> Data
-    static func decode(from data: Data) -> Self
+	
+	
+	/// Encode a type into data ready for storage.
+	///
+	/// - Returns: A data representation of the type.
+	func encode() -> Data
+	
+	
+	/// Decode data back into the type.
+	///
+	/// - Parameter data: The data from storage.
+	/// - Returns: A instance of the type as decoded from the data.
+	static func decode(from data: Data) -> Self
+}
+
+
+// MARK: - Default implementation
+extension Storage {
+	
+	func add(_ entity: Entity) {
+		let toStore = allAsData() + [entity.encode()]
+		setStored(toStore)
+	}
+	
+	func remove(_ entity: Entity) {
+		var toStore = allAsData()
+		guard let index = toStore.index(of: entity.encode()) else { return }
+		toStore.remove(at: index)
+		setStored(toStore)
+	}
+	
+	func all() -> [Entity] {
+		return allAsData().compactMap { Entity.decode(from: $0) }
+	}
+	
+	func reset() {
+		setStored([Data]())
+	}
+}
+
+
+// MARK: - Using UserDefaults as a storage mechanism
+fileprivate extension Storage {
+	
+	private func allAsData() -> [Data] {
+		if let current = UserDefaults.standard.array(forKey: Self.key) as? [Data] {
+			return current
+		}
+		return [Data]()
+	}
+	
+	private func setStored(_ toStore: [Data]) {
+		UserDefaults.standard.set(toStore, forKey: Self.key)
+		didUpdate(to: all())
+	}
 }
 
 extension Storage {
-    
-    func add(_ entity: Entity) {
-        let toStore = allAsData() + [entity.encode()]
-        setStored(toStore)
-    }
-    
-    func remove(_ entity: Entity) {
-        var toStore = allAsData()
-        guard let index = toStore.index(of: entity.encode()) else { return }
-        toStore.remove(at: index)
-        setStored(toStore)
-    }
-    
-    func all() -> [Entity] {
-        return allAsData().compactMap { Entity.decode(from: $0) }
-    }
-    
-    func reset() {
-        setStored([Data]())
-    }
-    
-    private func allAsData() -> [Data] {
-        let allData: [Data] = {
-            guard let current = UserDefaults.standard.array(forKey: Self.key) as? [Data] else { return [Data]() }
-            return current
-        }()
-        return allData
-    }
-    
-    private func setStored(_ toStore: [Data]) {
-        UserDefaults.standard.set(toStore, forKey: Self.key)
-        didUpdate(to: all())
-    }
-}
-
-extension Storage {
-    // Optional function
-    func didUpdate(to entities: [Entity]) { }
+	// Make function optional
+	func didUpdate(to entities: [Entity]) { }
 }
