6b2554a5564416755890275278108fddedb80474
Luke Stringer <luke@lukestringer.me>
2019-01-01 11:28:55 +0000

    Update cocoapods and add some explanatory comments

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,2 +1,5 @@
+# This fixes build problems on App Center (an xcodebuild error 65 to be specific)
 gem install cocoapods --pre
+
+# Install cocoapods keys on App Center, then use env vars in the build config to set the keys
 gem install cocoapods-keys
